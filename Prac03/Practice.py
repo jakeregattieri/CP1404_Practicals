ca9b96def3ef4da88b0e4597bcac6cae1f232b65
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-03-19 21:02:21 +1000

    Add practice example

--- Prac03/Practice.py
+++ Prac03/Practice.py
@@ -1,8 +1,8 @@
 INVALID_NAME = ""
 
-
 user_name = input("what is your name: ".strip())
 while user_name == INVALID_NAME:
     print("invalid name")
     user_name = input("what is your name: ".strip())
-for i in range(user_name):
+for char in user_name[::2]:
+    print(char)
