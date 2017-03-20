78afb162a1bf2ea48440a3a0980cc7b2a2c192f9
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-03-20 11:16:26 +1000

    Add totalIncome task

---
+++ Prac04/totalIncome.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    incomes = []
+    total_months = int(input("How many months? "))
+
+    for month in range(1, total_months + 1):
+        income = float(input("Enter income for month {}: ".format(month)))
+        incomes.append(income)
+    calculate_income_report(incomes, total_months)
+
+
+def calculate_income_report(incomes, total_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, total_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
