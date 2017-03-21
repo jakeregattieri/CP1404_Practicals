835cfb66760a967da605b7d9877870d67fa7f23f
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-03-21 13:53:02 +1000

    list comprehension TODO's updated

--- Prac04/listComprehensions.py
+++ Prac04/listComprehensions.py
@@ -1,4 +1,3 @@
-
 """
 CP1404/CP5632 Practical
 List comprehensions
@@ -22,7 +21,6 @@ print(first_initials)
 full_initials = [(name.split()[0][0], name.split()[1][0]) for name in full_names]
 print(full_initials)
 
-
 almost_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # TODO: use a list comprehension to create a list of integers from this list of strings
@@ -33,6 +31,8 @@ for number in almost_numbers:
     new_number += 1
 print(numbers)
 
+numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
 # TODO: use a list comprehension to create a list of all of the full_names in lowercase
 lowercase_full_names = [name.lower() for name in full_names]
