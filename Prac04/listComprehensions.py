54168a428bb0cf8176b5c561f5c05a3d028e9264
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-03-21 12:28:50 +1000

    Add list comprehension TODO's

--- Prac04/listComprehensions.py
+++ Prac04/listComprehensions.py
@@ -18,7 +18,7 @@ first_initials = [name[0] for name in names]
 print(first_initials)
 
 # list comprehension that creates a list containing tuples of both initials
-# splits each name and adds the first letter of each part to a tuple
+# splits each name and ad
 full_initials = [(name.split()[0][0], name.split()[1][0]) for name in full_names]
 print(full_initials)
 
@@ -26,7 +26,14 @@ print(full_initials)
 almost_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # TODO: use a list comprehension to create a list of integers from this list of strings
-# numbers =
+numbers = []
+new_number = 0
+for number in almost_numbers:
+    numbers.append(new_number)
+    new_number += 1
+print(numbers)
+
 
 # TODO: use a list comprehension to create a list of all of the full_names in lowercase
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
