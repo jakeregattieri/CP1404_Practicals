def4983aaf7d381c5840836138b595e3415655ac
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-04-12 17:49:19 +1000

    add miles_to_kilometres program

---
+++ Prac06/miles_to_kilometres.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+MILES_TO_KILOMETRES = 1.609347
+
+
+class MilesToKilometres(App):
+    def build(self):
+        Window.size = (500, 200)
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('miles_to_kilometres.kv')
+        return self.root
+
+    def handle_miles_to_kilometres(self):
+        miles = self.get_validated_miles()
+        kilometres = miles * MILES_TO_KILOMETRES
+        self.root.ids.output_kilometres.text = str(kilometres)
+
+    def handle_increment(self, change):
+        miles = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def get_validated_miles(self):
+        validated_miles = float(self.root.ids.input_miles.text)
+        return validated_miles
+
+
+MilesToKilometres().run()
